059a34a894415781adaeb21f3e3c4664c6cb3ec9
Alex <strelkovax@gmail.com>
2020-08-23 20:27:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mymovies/DetailActivity.java
@@ -0,0 +1,163 @@
+package com.example.mymovies;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mymovies.adapters.ReviewAdapter;
+import com.example.mymovies.adapters.TrailerAdapter;
+import com.example.mymovies.data.FavoriteMovie;
+import com.example.mymovies.data.MainViewModel;
+import com.example.mymovies.data.Movie;
+import com.example.mymovies.data.Review;
+import com.example.mymovies.data.Trailer;
+import com.example.mymovies.utils.JSONUtils;
+import com.example.mymovies.utils.NetworkUtils;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private ImageView imageViewBigPoster;
+    private TextView textViewTitle;
+    private TextView textViewOriginalTitle;
+    private TextView textViewRating;
+    private TextView textViewReleaseDate;
+    private TextView textViewOverview;
+    private ImageView imageViewAddToFavorite;
+    private ScrollView scrollViewInfo;
+
+    private RecyclerView recyclerViewTrailers;
+    private RecyclerView recyclerViewReviews;
+    private ReviewAdapter reviewAdapter;
+    private TrailerAdapter trailerAdapter;
+
+    private int id;
+    private Movie movie;
+    private FavoriteMovie favoriteMovie;
+
+    private MainViewModel viewModel;
+
+    private static String lang;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.itemMain:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.itemFavorite:
+                Intent intentToFavorite = new Intent(this, FavoriteActivity.class);
+                startActivity(intentToFavorite);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        lang = Locale.getDefault().getLanguage();
+        imageViewAddToFavorite = findViewById(R.id.imageViewAddToFavorite);
+        imageViewBigPoster = findViewById(R.id.imageViewBigPoster);
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewOriginalTitle = findViewById(R.id.textViewOriginalTitle);
+        textViewRating = findViewById(R.id.textViewRating);
+        textViewReleaseDate = findViewById(R.id.textViewReleaseDate);
+        textViewOverview = findViewById(R.id.textViewOverview);
+        imageViewBigPoster = findViewById(R.id.imageViewBigPoster);
+        scrollViewInfo = findViewById(R.id.scrollViewInfo);
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("id")) {
+            id = intent.getIntExtra("id", -1);
+        } else {
+            finish();
+        }
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        movie = viewModel.getMovieById(id);
+        if (movie == null) {
+            movie = viewModel.getFavoriteMovieById(id);
+        }
+        createMovieDetails(movie);
+        setFavorite();
+        recyclerViewTrailers = findViewById(R.id.recyclerViewTrailers);
+        recyclerViewReviews = findViewById(R.id.recyclerViewReviews);
+        reviewAdapter = new ReviewAdapter();
+        trailerAdapter = new TrailerAdapter();
+        trailerAdapter.setOnTrailerClickListener(new TrailerAdapter.OnTrailerClickListener() {
+            @Override
+            public void onTrailerClick(String url) {
+                Intent intentToTrailer = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentToTrailer);
+            }
+        });
+        recyclerViewReviews.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewTrailers.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewReviews.setAdapter(reviewAdapter);
+        recyclerViewTrailers.setAdapter(trailerAdapter);
+        JSONObject jsonObjectTrailers = NetworkUtils.getJSONForVideos(movie.getId(), lang);
+        JSONObject jsonObjectReviews = NetworkUtils.getJSONForReviews(movie.getId(), lang);
+        ArrayList<Trailer> trailers = JSONUtils.getTrailersFromJSON(jsonObjectTrailers);
+        ArrayList<Review> reviews = JSONUtils.getReviewsFromJSON(jsonObjectReviews);
+        reviewAdapter.setReviews(reviews);
+        trailerAdapter.setTrailers(trailers);
+        scrollViewInfo.smoothScrollTo(0,0);
+    }
+
+    public void onClickChangeFavorite(View view) {
+        if (favoriteMovie == null) {
+            viewModel.insertFavoriteMovie(new FavoriteMovie(movie));
+            Toast.makeText(this, R.string.add_to_favorite, Toast.LENGTH_SHORT).show();
+        } else {
+            viewModel.deleteFavoriteMovie(favoriteMovie);
+            Toast.makeText(this, R.string.remove_from_favorite, Toast.LENGTH_SHORT).show();
+        }
+        setFavorite();
+    }
+
+    private void setFavorite() {
+        favoriteMovie = viewModel.getFavoriteMovieById(id);
+        if (favoriteMovie == null) {
+            imageViewAddToFavorite.setImageResource(R.drawable.staroff);
+        } else {
+            imageViewAddToFavorite.setImageResource(R.drawable.staron);
+        }
+    }
+
+    private void createMovieDetails(Movie movie) {
+        Picasso.get().load(movie.getBigPosterPath()).placeholder(R.drawable.nonetwork).into(imageViewBigPoster);
+        textViewTitle.setText(movie.getTitle());
+        textViewOriginalTitle.setText(movie.getOriginalTitle());
+        textViewRating.setText(Double.toString(movie.getVoteAverage()));
+        textViewReleaseDate.setText(movie.getReleaseDate());
+        textViewOverview.setText(movie.getOverview());
+    }
+}
